5024c5840083757a880894b370fdb0e867e62fb5
jfuertes <jfuertesl2@gmail.com>
2016-02-25 18:48:17 -0500

    agrega parametro en ambas tablas

--- admin/api/selectParamByProyMacro.php
+++ admin/api/selectParamByProyMacro.php
@@ -7,9 +7,12 @@
 
 	$rspta = json_decode(file_get_contents("php://input"));
 	$IDPROYMACRO= $rspta->pm->idProy;
-	$q = 'SELECT pmp.IDPARAMETRO, par.IDTIPODATO, par.NOMBREPARAM, par.USAMAESTROPARAM, par.ESTADOPARAM
-			from pmparametro pmp
-			INNER JOIN parametro par on pmp.IDPARAMETRO = par.IDPARAMETRO
+	$q = 'SELECT pmp.IDPARAMETRO , par.NOMBREPARAM, mo.NOMBREMODULO, td.NOMBRETIPODATO, par.ESTADOPARAM,  pmp.ORDEN, par.USAMAESTROPARAM, pm.NOMBREPROYMACRO
+		from pmparametro pmp
+		INNER JOIN parametro par on pmp.IDPARAMETRO = par.IDPARAMETRO
+		INNER JOIN modulo mo on mo.IDMODULO = pmp.IDMODULO
+		INNER JOIN tipodato td on td.IDTIPODATO = par.IDTIPODATO
+		INNER JOIN proymacro pm on pm.IDPROYMACRO = pmp.IDPROYMACRO
 			where pmp.IDPROYMACRO= :IDPROYMACRO;';
 
 	$stmt = $dbh->prepare($q);
