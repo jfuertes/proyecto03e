3802e36247805012ae111ca313b9a39712ecf51a
jfuertes <jfuertesl2@gmail.com>
2016-03-17 17:16:29 -0500

    sa

--- admin/api/addAcceso.php
+++ admin/api/addAcceso.php
@@ -12,26 +12,56 @@
 	$acc= $rspta->acc;
 	$IDUSER= $rspta->idpa;
 	//var_dump($etiqueta);
-
-	$q = 'SELECT max(IDACCESO) +1 as IDACCESO from proyred.ACCESO';
-	$stmt = $dbh->prepare($q);
-	$stmt->execute();
-	$r=$stmt->fetch(PDO::FETCH_ASSOC);
-
-	$IDACCESO=$r['IDACCESO'];
-
-	$q = 'INSERT INTO proyred.ACCESO (IDACCESO, IDPROYMACRO, IDMODULO, IDUSUARIO, TIPOUS, PRIVILEGIO) 
-				values (:IDACCESO, :IDPROYMACRO, :IDMODULO, :IDUSUARIO, :TIPOUS, :PRIVILEGIO)';
-		
+	//verificar que acceso no este repetido
+	$q = 'SELECT * from proyred.ACCESO where IDPROYMACRO= :IDPROYMACRO
+			 and  IDMODULO=:IDMODULO and IDUSUARIO=:IDUSUARIO ';
 		$stmt = $dbh->prepare($q);
-		$stmt->bindParam(':IDACCESO',  $IDACCESO, PDO::PARAM_STR);
 		$stmt->bindParam(':IDPROYMACRO',  $acc->IDPROYMACRO, PDO::PARAM_STR);
 		$stmt->bindParam(':IDMODULO',  $acc->IDMODULO, PDO::PARAM_STR);
 		$stmt->bindParam(':IDUSUARIO',  $IDUSER, PDO::PARAM_STR);
-		$stmt->bindParam(':TIPOUS',  $acc->TIPOUS, PDO::PARAM_STR);
-		$stmt->bindParam(':PRIVILEGIO',  $acc->PRIVILEGIO, PDO::PARAM_STR);
 
-		$valor = $stmt->execute();
-		echo json_encode($valor);
+		$stmt->execute();
+		$valor = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+		/*echo $acc->IDPROYMACRO;
+		echo $acc->IDMODULO;
+		echo $acc->TIPOUS;
+		echo $acc->PRIVILEGIO;
+		var_dump($valor);*/
+		if(count($valor)!=0){
+				$rpta=array('Error' => 'Error: ya se encuentra registrado ese acceso, primero eliminelo.');
+		}
+		else{
+			$q = 'SELECT max(IDACCESO) +1 as IDACCESO from proyred.ACCESO';
+				$stmt = $dbh->prepare($q);
+				$stmt->execute();
+				$r=$stmt->fetch(PDO::FETCH_ASSOC);
+
+				$IDACCESO=$r['IDACCESO'];
+
+			$q = 'INSERT INTO proyred.ACCESO (IDACCESO, IDPROYMACRO, IDMODULO, IDUSUARIO, TIPOUS, PRIVILEGIO) 
+				values (:IDACCESO, :IDPROYMACRO, :IDMODULO, :IDUSUARIO, :TIPOUS, :PRIVILEGIO)';
+				
+				$stmt = $dbh->prepare($q);
+				$stmt->bindParam(':IDACCESO',  $IDACCESO, PDO::PARAM_STR);
+				$stmt->bindParam(':IDPROYMACRO',  $acc->IDPROYMACRO, PDO::PARAM_STR);
+				$stmt->bindParam(':IDMODULO',  $acc->IDMODULO, PDO::PARAM_STR);
+				$stmt->bindParam(':IDUSUARIO',  $IDUSER, PDO::PARAM_STR);
+				$stmt->bindParam(':TIPOUS',  $acc->TIPOUS, PDO::PARAM_STR);
+				$stmt->bindParam(':PRIVILEGIO',  $acc->PRIVILEGIO, PDO::PARAM_STR);
+
+				$valor = $stmt->execute();
+				if($valor=true){
+					$rpta=array('success' => 'El área fue creada exitosamente');
+				}
+				else{
+					$rpta=array('Error' => 'Error: Intentelo mas tarde la base de datos no responde.');
+				}
+			
+		}
+
+
+echo json_encode($rpta);
+	
 
 ?>
